1dc892f6782b57994497263f2678836857d17b4f
ahmedmohsen302 <ahmedthemohsen@gmail.com>
2025-06-02 20:55:42 +0300

    created splash view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:book_store_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const BookStorApp());
@@ -9,6 +11,6 @@ class BookStorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const GetMaterialApp(home: SplashView());
   }
 }
